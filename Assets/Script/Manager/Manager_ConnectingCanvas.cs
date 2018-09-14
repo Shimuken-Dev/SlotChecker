47d7aedb91c2d71ef358aa6284e8ece1a9933b30
Knowable-Kenta-Shimura <kenta_shimura@knowable.jp>
2018-09-15 00:02:52 +0900

    [add] 通信中画面を作成

---
+++ Assets/Script/Manager/Manager_ConnectingCanvas.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager_ConnectingCanvas : MonoBehaviour {
+
+	GameObject Connecting_obj;
+	Text Connecting_text;
+	bool Flg_Animation;
+
+
+	void Start(){
+		Initialize ();
+	}
+
+
+//パブリック関数
+	//通信開始
+	public void Start_Connecting(){
+		Connecting_obj.SetActive (true);
+		Flg_Animation = true;
+		StartCoroutine (Animation_Text ());
+	}
+	//通信完了
+	public void Stop_Connecting(){
+		Flg_Animation = false;
+		Connecting_obj.SetActive (false);
+	}
+
+
+//関数
+	//初期化
+	void Initialize(){
+		Flg_Animation = false;
+		Connecting_obj = gameObject.transform.Find ("ConnectingImg").gameObject;
+		Connecting_text = Connecting_obj.transform.Find ("Text").GetComponent<Text> ();
+	}
+	//コネクトアニメーション
+	IEnumerator Animation_Text (){
+		while(Flg_Animation){
+			switch(Connecting_text.text){
+			case "通信中":
+				Connecting_text.text = "通信中.";
+			break;
+			case "通信中.":
+				Connecting_text.text = "通信中..";
+				break;
+			case "通信中..":
+				Connecting_text.text = "通信中...";
+				break;
+			case "通信中...":
+				Connecting_text.text = "通信中";
+				break;
+			}
+			yield return new WaitForSeconds (1f);
+		}
+		yield break;
+	}
+}
---
+++ Assets/Script/etc/CommonFunctionsCtr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonFunctionsCtr : MonoBehaviour {
+
+	//**** stringをUnescapeして返す関数 ****//
+	public static string UnEscape (string str){
+		if (!string.IsNullOrEmpty (str)) {
+			string finish_str = System.Text.RegularExpressions.Regex.Unescape (str);
+			return finish_str;
+		} else {
+			return "Null";
+		}
+	}
+}
