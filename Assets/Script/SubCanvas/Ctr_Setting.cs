07b913ac04e10cbb50a25b7900888d598301af0d
Knowable-Kenta-Shimura <kenta_shimura@knowable.jp>
2018-09-29 16:29:29 +0900

    [add] 設定画面の各ボタン効果を追加

--- Assets/Script/SubCanvas/Ctr_Setting.cs
+++ Assets/Script/SubCanvas/Ctr_Setting.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using NCMB;
 
 public class Ctr_Setting : MonoBehaviour {
 
@@ -11,6 +12,7 @@ public class Ctr_Setting : MonoBehaviour {
 	MailAddress_Text;
 
 	Manager_SubCanvas SubCanvasMng;
+	Manager_MessageCanvas MessageCanvasMng;
 
 	void Start(){
 		Initialized ();
@@ -23,6 +25,7 @@ public class Ctr_Setting : MonoBehaviour {
 		UserName_Text.text = Data_User.User_Name;
 		MailAddress_Text.text = Data_User.Mail_Address;
 		SubCanvasMng = GameObject.Find ("SubCanvas").GetComponent<Manager_SubCanvas> ();
+		MessageCanvasMng = GameObject.Find ("MessageCanvas").GetComponent<Manager_MessageCanvas> ();
 	}
 
 
@@ -30,5 +33,27 @@ public class Ctr_Setting : MonoBehaviour {
 	//閉じるボタン
 	public void OnClick_CloseBtn(){
 		SubCanvasMng.CloseSub (false, gameObject.GetComponent<RectTransform> ());
+	}
+	//パスワード変更ボタン
+	public void OnClick_PasswordChangeBtn(){
+		NCMBUser.RequestPasswordResetAsync (Data_User.Mail_Address, (error) => {
+			if (error != null) {
+				// エラー処理
+				MessageCanvasMng.OpenMessagePopup (Manager_MessageCanvas.PopUpType.Normal, "サーバー通信でエラーが発生しました");
+			} else {
+				// 送信後処理
+				MessageCanvasMng.OpenMessagePopup (Manager_MessageCanvas.PopUpType.Normal, "登録されているメールアドレス宛に\nメールを送信致しました\nそちらのメールから変更を行って下さい");
+			}
+		});
+
+	}
+	//再ログインボタン
+	public void OnClick_ReLoginBtn(){
+		NCMBUser.LogOutAsync ();
+		SubCanvasMng.OpenSub (Manager_SubCanvas.Sub.Login);
+	}
+	//お問い合わせボタン
+	public void OnClick_ContactBtn(){
+		OpenMailerCtr.Open ();
 	}
 }
