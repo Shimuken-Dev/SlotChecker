9c4417ec1b6b942024ed25e5b3e4723aa469918a
Knowable-Kenta-Shimura <kenta_shimura@knowable.jp>
2018-10-02 16:44:52 +0900

    [add] スロットリストメニューの閉じるボタンを追加

---
+++ Assets/Script/SubCanvas/Ctr_SlotListMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctr_SlotListMenu : MonoBehaviour {
+
+
+	Manager_SubCanvas SubCanvasMng;
+
+
+	void Awake (){
+		Initialize ();
+	}
+
+	//関数
+	void Initialize(){
+		SubCanvasMng = GameObject.Find ("SubCanvas").GetComponent<Manager_SubCanvas> ();
+	}
+
+//UGUI
+	//閉じるボタン
+	public void OnClick_CloseBtn(){
+		SubCanvasMng.CloseSub (false, gameObject.GetComponent<RectTransform> ());
+	}
+}
