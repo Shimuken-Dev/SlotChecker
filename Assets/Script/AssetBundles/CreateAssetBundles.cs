bdadc38a5ec73f6a3745194ce5ce701523a56cb3
Knowable-Kenta-Shimura <kenta_shimura@knowable.jp>
2018-10-04 18:33:59 +0900

    [change] Androidでビルドする為の設定を適切な形へ変更した

--- Assets/Script/AssetBundles/CreateAssetBundles.cs
+++ Assets/Script/AssetBundles/CreateAssetBundles.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEditor;
 
 public class CreateAssetBundles {
-	[MenuItem ("Assets/Build AssetBundles")]
-	static void BuildAllAssetBundles ()
-	{
-		// Android用
-		BuildPipeline.BuildAssetBundles ("Assets/AssetBundles/Android", BuildAssetBundleOptions.None, BuildTarget.Android);
-		// iOS用
-		BuildPipeline.BuildAssetBundles ("Assets/AssetBundles/iOS", BuildAssetBundleOptions.None, BuildTarget.iOS);
-	}
+	//[MenuItem ("Assets/Build AssetBundles")]
+	//static void BuildAllAssetBundles ()
+	//{
+	//	// Android用
+	//	BuildPipeline.BuildAssetBundles ("Assets/AssetBundles/Android", BuildAssetBundleOptions.None, BuildTarget.Android);
+	//	// iOS用
+	//	BuildPipeline.BuildAssetBundles ("Assets/AssetBundles/iOS", BuildAssetBundleOptions.None, BuildTarget.iOS);
+	//}
 }
