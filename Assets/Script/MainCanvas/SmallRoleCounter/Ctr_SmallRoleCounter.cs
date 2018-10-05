a3218fb6f78eece2c113c430d56412f42694d8cf
Knowable-Kenta-Shimura <kenta_shimura@knowable.jp>
2018-10-05 17:01:46 +0900

    [add] 小役カウンターページを作成

---
+++ Assets/Script/MainCanvas/SmallRoleCounter/Ctr_SmallRoleCounter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctr_SmallRoleCounter : MonoBehaviour {
+
+	[SerializeField]
+	Transform
+	CounterTrns;
+
+	[SerializeField]
+	GameObject 
+	BackBtnObj,
+	CloseBtnObj;
+
+	Manager_FadeCanvas FadeCanvasMng;
+	Manager_MainCanvas MainCanvasMng;
+
+	Ctr_SmallRoleCounter_Popup PopupCtr;
+
+	void Start (){
+		MainCanvasMng = GameObject.Find ("MainCanvas").GetComponent<Manager_MainCanvas> ();
+		FadeCanvasMng = GameObject.Find ("FadeCanvas").GetComponent<Manager_FadeCanvas> ();
+		PopupCtr = gameObject.transform.Find ("MenuPopup").GetComponent<Ctr_SmallRoleCounter_Popup> ();
+		Initialize ();
+		StartCoroutine (FadeCanvasMng.FadeOut (() => PopupCtr.Open (Ctr_SmallRoleCounter_Popup.Type.Counter_Select)));
+	}
+
+	void OnEnable (){
+		Initialize ();
+	}
+
+	//関数
+	void Initialize(){
+		BackBtnObj.SetActive (false);
+		CloseBtnObj.SetActive(true);
+	}
+
+
+//UGUI
+
+	//閉じるボタン
+	public void OnClick_CloseBtn(){
+		StartCoroutine (FadeCanvasMng.FadeIn (() => MainCanvasMng.OpenMain (Manager_MainCanvas.Main.Title)));
+	}
+	//戻るボタン
+	public void OnClick_BackBtn(){
+		if(CounterTrns.childCount != 0){
+			foreach(Transform child in CounterTrns){
+				Destroy (child.gameObject);
+			}
+		}
+		BackBtnObj.SetActive (false);
+		CloseBtnObj.SetActive (true);
+		PopupCtr.Open (Ctr_SmallRoleCounter_Popup.Type.Counter_Select);
+	}
+}
---
+++ Assets/Script/MainCanvas/SmallRoleCounter/Ctr_SmallRoleCounter_Popup.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Ctr_SmallRoleCounter_Popup : MonoBehaviour {
+
+	[SerializeField]
+	Transform
+	CounterTrns;
+
+	[SerializeField]
+	GameObject
+	BackBtnObj,
+	CloseBtnObj;
+
+	public enum Type{
+		Counter_Select
+	}
+
+
+	public void Open(Type type){
+		switch(type){
+		case Type.Counter_Select:
+			PopupAnimation (true);
+			break;
+		}
+	}
+
+	public void Close(){
+		PopupAnimation (false);
+	}
+
+//関数
+
+	//Popupアニメーション関数
+	void PopupAnimation (bool flg){
+		//Open
+		if (flg == true) {
+			gameObject.GetComponent<RectTransform>().DOScale (Vector3.one, 0.5f)
+				 .SetEase (Ease.OutBack);
+		}
+		//Close
+		else {
+			gameObject.GetComponent<RectTransform> ().DOScale (Vector3.zero, 0.5f)
+				 .SetEase (Ease.InBack)
+				 .OnComplete (() => {
+					 // アニメーションが終了時によばれる
+				 });
+		}
+	}
+
+//UGUI
+
+	//通常カウンターを使用するボタン
+	public void OnClick_TemplateBtn (){
+		GameObject Counter = Instantiate (Resources.Load ("Prefab/SmallRoleCounter/Default") as GameObject);
+		Counter.transform.SetParent (CounterTrns, false);
+		Close ();
+		BackBtnObj.SetActive (true);
+		CloseBtnObj.SetActive (false);
+	}
+	//オリジナルカウンター作成ボタン
+	public void OnClick_OriginalBtn (){
+		//TODO
+		Close ();
+		BackBtnObj.SetActive (true);
+		CloseBtnObj.SetActive (false);
+	}
+}
